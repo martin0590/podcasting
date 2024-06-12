8ad81b787d5c41b112377db9c110d0ea8f39f58e
Martin Alejandro Mammana <martin.ale590@gmail.com>
2024-06-12 15:36:28 -0300

    [feature] - added auth & DB Connectivity

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://witty-python-65.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://witty-python-65.clerk.accounts.dev",
+      applicationID: "convex",
+    }
+  ]
+}
+
+export default authConfig
