70f1350a2cb34a1705fd091f658dc1ccf4fd7edc
Martin Alejandro Mammana <martin.ale590@gmail.com>
2024-06-13 14:27:45 -0300

    [edited] - create podcast page & connection to AI

---
+++ components/GenerateThumbnail.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./ui/button"
+
+const GenerateThumbnail = () => {
+  return (
+    <div className='generate_thummbnail'>
+      <Button type="button" variant='plain' className="bg-black-6">
+
+      </Button>
+    </div>
+  )
+}
+
+export default GenerateThumbnail
